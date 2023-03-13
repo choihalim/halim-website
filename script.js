ace5d439151479f57d8f18e51aa1efee86d28999
Halim Choi <hchoicareer@gmail.com>
2023-03-13 14:05:23 -0400

    created about section, added css

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ function showProfilePic() {
     });
 }
 
+const aboutArea = document.getElementById("about");
+
+function showDescription() {
+    aboutArea.addEventListener("mouseover", function() {
+        aboutArea.innerHTML = "";
+        const description = document.createElement("p");
+        description.className = "desc";
+        description.innerHTML = "Hello, my name is Halim Choi. I studied Computer Science at Boston College (C/O '22) and I am excited to begin my career as a Software Engineer. ";
+        aboutArea.appendChild(description);
+    });
+
+    aboutArea.addEventListener("mouseout", function() {
+        aboutArea.innerHTML = "About Me";
+        aboutArea.removeChild(description);
+    });
+}
+
 showProfilePic();
+showDescription();
